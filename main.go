334f1913dfcb5ca284f41f5376ba0b2f4fcd30a9
Yavuz <m.yavuzyagis@gmail.com>
2022-02-11 12:24:47 +0300

    write respose body if err

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		data, err := ioutil.ReadAll(r.Body)
 		if err != nil {
-			log.Fatal(err)
+			rw.WriteHeader(http.StatusBadRequest)
 		}
 		log.Printf("Data: %s", data)
 		fmt.Fprintf(rw, "Hello, %s", data)
