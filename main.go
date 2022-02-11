23b8deabb3d7fc5fb4143c8b1ba22a7c66d13eed
Yavuz <m.yavuzyagis@gmail.com>
2022-02-11 16:19:05 +0300

    Fix some bugs and add some features. THIS IS A GENERIC AUTOMATED Commit

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/MYavuzYAGIS/GoPipe/handlers"
 )
@@ -21,7 +22,18 @@ func main() {
 	// Mux implementation
 	serveMux.Handle("/hello", helloHandler)
 	serveMux.Handle("/goodbye", goodbyeHandler)
-	// Serve
-	http.ListenAndServe(":9090", serveMux)
+
+	//Createing custom Server properties to better fine-tune the server details and run away from possible DDOS attacks by setting
+	//the timeout and the read and write timeouts
+	server := &http.Server{
+		Addr:         ":9090",
+		Handler:      serveMux,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	// Serve the server and replace the default http.ListenAndServe() with the custom server
+	server.ListenAndServe()
 
 }
