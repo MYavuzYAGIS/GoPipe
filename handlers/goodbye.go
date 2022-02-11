10c828852e28dd238b9baec9d9aab814a72e483c
Yavuz <m.yavuzyagis@gmail.com>
2022-02-11 15:27:42 +0300

    Fix some bugs and add some features. THIS IS A GENERIC AUTOMATED Commit

---
+++ handlers/goodbye.go
@@ -0,0 +1,26 @@
+package handlers
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Goodbye struct {
+	l *log.Logger
+}
+
+func NewGoodbye(l *log.Logger) *Goodbye {
+	return &Goodbye{l}
+}
+
+func (h *Goodbye) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	h.l.Println("Goodbye ")
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(rw, "Ooops", http.StatusBadRequest)
+		return
+	}
+	fmt.Fprintf(rw, " Welcome to My World. This is a Go Server I made using ServeMux and HttpHandlers.  %s", data)
+}
--- main.go
+++ main.go
@@ -12,9 +12,16 @@ func main() {
 	// http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 	// })
 	l := log.New(os.Stdout, "product-API", log.LstdFlags)
+
+	//Handler defs
 	helloHandler := handlers.NewHello(l)
+	goodbyeHandler := handlers.NewGoodbye(l)
+	// Mux defs
 	serveMux := http.NewServeMux()
+	// Mux implementation
 	serveMux.Handle("/hello", helloHandler)
+	serveMux.Handle("/goodbye", goodbyeHandler)
+	// Serve
 	http.ListenAndServe(":9090", serveMux)
 
 }
