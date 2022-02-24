fcc4f1f28b09243e296430d31553f0f93990cb84
MYavuzYAGIS <m.yavuzyagis@gmail.com>
2022-02-24 15:39:46 +0300

    add get post handlers

--- handlers/products.go
+++ handlers/products.go
@@ -1,9 +1,10 @@
 package handlers
 
 import (
-	"github.com/MYavuzYAGIS/GoPipe/data"
 	"log"
 	"net/http"
+
+	"github.com/MYavuzYAGIS/GoPipe/data"
 )
 
 type Products struct {
@@ -14,8 +15,18 @@ func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
-func (p *Products) ServeHTTP(rw http.ResponseWriter, h *http.Request) {
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		p.getProducts(rw, r)
+		return
+	}
+	// catch all other methods
 
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+
+}
+
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
 	listProducts := data.GetProducts()
 	err := listProducts.ToJSON(rw)
 	if err != nil {
